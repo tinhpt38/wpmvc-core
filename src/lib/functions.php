2d06ae6432d9f3866864b0f493defdd162ebd7ab
Alejandro Mostajo <amostajo@gmail.com>
2018-02-04 16:50:41 -0600

    Improved resize_image function.
    `resize_image()` now supports ID as parameter to make resulting image
    more unique. With out it there is a chance and image is not created if
    there is another one with the same name.

--- src/lib/functions.php
+++ src/lib/functions.php
@@ -15,45 +15,42 @@ use WPMVC\Commands\SetCommand;
  * @copyright 10Quality <http://www.10quality.com>
  * @license MIT
  * @package WPMVC
- * @version 2.0.13
+ * @version 2.0.14
  */
 
 if ( ! function_exists( 'resize_image' ) ) {
     /**
      * Resizes image and returns URL path.
      * @since 1.0.1
+     * @since 2.0.14 ID added as parameter to prevent duplicate entries by the same name.
      *
      * @param string  $url    Image URL path
      * @param int     $width  Width wanted.
      * @param int     $height Height wanted.
      * @param boolean $crop   Flag that indicates if resulting image should crop
+     * @param mixed   $id     Unique identifier. (i.e. post_id)
      *
      * @return string URL
      */
-    function resize_image( $url, $width, $height, $crop = true )
+    function resize_image( $url, $width, $height, $crop = true, $id = null )
     {
         $image = wp_get_image_editor( $url );
-
         if( is_wp_error( $image ) ) return;
-
         $image_name = explode( '/', $url );
         $image_name = explode( '.', $image_name[count( $image_name ) - 1] );
         $image_extension = strtolower( $image_name[count( $image_name ) - 1] );
         $image_name = $image_name[0];
-
         $upload_dir = wp_upload_dir();
-
         $filename = sprintf(
-            '/%s-%sx%s.%s',
+            '/%s%s-%sx%s.%s',
+            ( $id === null ? '' : $id . '-' ),
             $image_name,
             $width,
             $height,
             $image_extension
         );
-
         $image->resize( $width, $height, $crop );
         $image->save( $upload_dir['path'] . $filename );
-
         return $upload_dir['url'] . $filename;
     }
 }
